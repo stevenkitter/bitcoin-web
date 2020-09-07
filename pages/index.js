3b1f6c3d6a3de4997a2d9f3d82ccd8880eab6df0
wangxu <stevenkitter@163.com>
2020-09-07 22:56:30 +0800

    wechat

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 // import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [isWeixin, setIsWeixin] = useState(false);
+  const isWeixinFunc = () => {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  useEffect(() => {
+    var isWeixin = isWeixinFunc();
+    setIsWeixin(isWeixin);
+  }, []);
   return (
     <div>
       <Head>
@@ -16,6 +30,7 @@ export default function Home() {
         }}
       >
         <h1 style={{ fontSize: 18 }}>比特币神器 ｜ 神奇的力量</h1>
+        <p>{isWeixin ? "微信用户请在游览器中打开" : ""}</p>
         <div className="columns">
           <div className="column">
             <div style={{ width: "100%", height: "auto" }}>
